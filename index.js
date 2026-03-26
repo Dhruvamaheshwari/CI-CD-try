c1a86832d81379dcf5ee501f1eb18a8e2e7a7e9c
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-03-27 02:00:56 +0530

    fix: bind to 0.0.0.0

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/'  ,(req , res) => {
     res.send('Hello jeee server run ho rha h => Dhruva Maheshwari')
 })
 
-app.listen(process.env.PORT || 4000 , ()=>console.log('server is runn on port 4000'))
+app.listen(process.env.PORT || "0.0.0.0" , ()=>console.log('server is runn on port 4000'))
